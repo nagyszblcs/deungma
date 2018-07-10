7e0951ff6970839674389c723114daf605511768
Szabolcs Nagy <nagyszblcs@gmail.com>
2018-07-10 15:20:43 +0200

    static file path updated

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,5 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
+STATIC_URL = '/home/nagyszblcs/nagyszblcs.pythonanywhere.com/blog/static'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
