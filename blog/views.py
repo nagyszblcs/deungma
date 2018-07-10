580ec74134f477589e1a9d7acda7557559d3e5f1
Szabolcs Nagy <nagyszblcs@gmail.com>
2018-07-10 14:42:56 +0200

    Dynamic data from db in template

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from blog.models import Post
+from django.utils import timezone
+
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    print(posts)
+    return render(request, 'blog/post_list.html', {'posts': posts})
