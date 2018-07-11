feeb469836c27766b83508c4460193d9b4b5c528
Szabolcs Nagy <nagyszblcs@gmail.com>
2018-07-11 09:45:03 +0200

    Added favicon

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,7 @@
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
+
 from . import views
 
 urlpatterns = [
@@ -6,5 +9,8 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
-
 ]
+
+if settings.DEBUG:
+    #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
